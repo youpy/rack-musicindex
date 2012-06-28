55f518a0c80ccaa1a944c2e991bba373b9ae5488
youpy <youpy@buycheapviagraonlinenow.com>
2012-06-29 02:03:23 +0900

    refactor

--- spec/rack/musicindex_spec.rb
+++ spec/rack/musicindex_spec.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../test_app')
+%w(/../spec_helper /../test_app).each do |filename|
+  require File.expand_path(File.dirname(__FILE__) + filename)
+end
 
 set :environment, :test
 
